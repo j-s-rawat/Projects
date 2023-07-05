44389ac56e89790fa942e6a835bd8a4ae5a31d27
jitendar <jitendar.rawat@bps.uk.atos.net>
2023-07-05 18:49:00 +0100

    added UT

--- go/lists/list_intersection.go
+++ go/lists/list_intersection.go
@@ -19,40 +19,74 @@ func DoLinkedListsIntersect(nodes []*Node, test string) bool {
 
 	for _, val := range splits {
 		//look for the node matching the value
-		idx := SearchNodes(nodes, val)
-		if idx == -1 {
+		dp := SearchNodes(nodes, val)
+		if dp == nil {
 			continue //element not in list
 		}
 
-		node := nodes[idx]
 		//from this node go to the end of list and add all element to the map and set the value as 1
-		for node.next != nil {
-			if tempMap[node.val] == 1 {
+		for dp.next != nil {
+			if tempMap[dp.val] == 1 {
 				return true
 			} else {
-				tempMap[node.val] = 1
+				tempMap[dp.val] = 1
 			}
-			node = node.next
+			dp = dp.next
 		}
 		//last node
-		if tempMap[node.val] == 1 {
+		if tempMap[dp.val] == 1 {
 			return true
 		} else {
-			tempMap[node.val] = 1
+			tempMap[dp.val] = 1
 		}
 
 	}
 	return false
 }
 
-// Note: this function can be improved to return the node and not the index
-func SearchNodes(nodes []*Node, val string) (index int) {
-	for index, node := range nodes {
+func SearchNodes(nodes []*Node, val string) *Node {
+	for _, node := range nodes {
 		if node.val == val {
-			return index
+			return node
 		}
 	}
-	return -1
+	return nil
+}
+
+func buildList(s, d string, allNodes []*Node) []*Node {
+	dest := Node{
+		val: d,
+	}
+	src := Node{
+		val:  s,
+		next: &dest,
+	}
+	sp := SearchNodes(allNodes, src.val)
+	dp := SearchNodes(allNodes, dest.val)
+	if sp == nil && dp == nil { //source and dest are new so a new list
+		allNodes = append(allNodes, &src)
+		allNodes = append(allNodes, &dest)
+		return allNodes
+	} else if sp != nil && dp == nil { //source is found in all node collection and dest is missing, append dest to the end of existing source
+		sp.next = &dest
+		allNodes = append(allNodes, &dest)
+	} else if sp == nil && dp != nil { //source is not in allNodes collection but dest is present, make a link between source and existing node
+		allNodes = append(allNodes, &src)
+		src.next = dp
+	} else { //both sourse and dest are found, just make a link between them
+		sp.next = dp
+		//if both source and dest are found once the link is made, check if from the dest we find the end of list of destination itself
+		val := dp.val
+		node := dp.next
+		for node.next != nil {
+			if node.val == val { //if we reach a node that has same value as dest its is cyclic
+				fmt.Println("Error Thrown!")
+				os.Exit(0)
+			}
+			node = node.next
+		}
+	}
+	return allNodes
 }
 
 func main() {
@@ -67,39 +101,8 @@ func main() {
 		text := scanner.Text()
 		if strings.Contains(text, "->") { //nodes of list
 			splits := strings.Split(text, "->")
-			dest := Node{
-				val: splits[1],
-			}
-			src := Node{
-				val:  splits[0],
-				next: &dest,
-			}
-			iSrc := SearchNodes(allNodes, src.val)
-			iDest := SearchNodes(allNodes, dest.val)
-			if iSrc < 0 && iDest < 0 { //source and dest are new so a new list
-				allNodes = append(allNodes, &src)
-				allNodes = append(allNodes, &dest)
-				continue
-			} else if iSrc >= 0 && iDest < 0 { //source is found in all node collection and dest is missing, append dest to the end of existing source
-				allNodes[iSrc].next = &dest
-				allNodes = append(allNodes, &dest)
-			} else if iSrc < 0 && iDest >= 0 { //source is not in allNodes collection but dest is present, make a link between source and existing node
-				allNodes = append(allNodes, &src)
-				src.next = allNodes[iDest]
-			} else { //both sourse and dest are found, just make a link between them
-				allNodes[iSrc].next = allNodes[iDest]
-				//if both source and dest are found once the link is made, check if from the dest we find the end of list of destination itself
-				val := allNodes[iDest].val
-				node := allNodes[iDest].next
-				for node.next != nil {
-					if node.val == val { //if we reach a node that has same value as dest its is cyclic
-						fmt.Println("Error Thrown!")
-						os.Exit(0)
-					}
-					node = node.next
-				}
+			allNodes = buildList(splits[0], splits[1], allNodes)
 
-			}
 		} else { //the string to test
 			if DoLinkedListsIntersect(allNodes, text) {
 				fmt.Println("True")
